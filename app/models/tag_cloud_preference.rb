7f0b388e9f803029834fe02fec8e1f252e28f415
KozhevnikovSPB <57673312+KozhevnikovSpb@users.noreply.github.com>
2026-08-25 00:09:32 +0300

    0.0.5: hide system Tags cloud per user and project

--- app/models/tag_cloud_preference.rb
+++ app/models/tag_cloud_preference.rb
@@ -8,4 +8,35 @@ class TagCloudPreference < ActiveRecord::Base
   validates :visible, inclusion: { in: [true, false] }
 
   # position is optional (user-specific order of clouds); nil = use project order
+
+  SYSTEM_HIDDEN_KEY = 'system_tag_cloud_hidden_project_ids'
+
+  class << self
+    def system_visible_for?(user, project)
+      return true unless user&.logged? && project
+
+      !system_hidden_project_ids(user).include?(project.id)
+    end
+
+    def set_system_visible!(user, project, visible)
+      return false unless user&.logged? && project
+
+      ids = system_hidden_project_ids(user)
+      if visible
+        ids.delete(project.id)
+      else
+        ids |= [project.id]
+      end
+      user.pref[SYSTEM_HIDDEN_KEY] = ids
+      user.pref.save
+    end
+
+    def system_hidden_project_ids(user)
+      return [] unless user&.logged? && user.pref
+
+      Array(user.pref[SYSTEM_HIDDEN_KEY]).map(&:to_i)
+    rescue StandardError
+      []
+    end
+  end
 end
