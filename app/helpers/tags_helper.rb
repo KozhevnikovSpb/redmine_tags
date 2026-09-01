e35da2f8b7ec07a0e99e0d178bb9fc58fb6d422d
KozhevnikovSPB <57673312+KozhevnikovSpb@users.noreply.github.com>
2026-09-01 22:54:25 +0300

    Revert cloud weight CSS/helper changes; leave display as before.

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -99,11 +99,7 @@ module TagsHelper
       add_tags(style, tags, content, item_el, options)
     end
 
-    wrap_class = ['tags-cloud', 'tags']
-    wrap_class << 'tags-cloud-simple' if style == :simple_cloud
-    wrap_class << 'tags-cloud-weighted' if style == :cloud
-    wrap_style = style == :simple_cloud ? 'text-align: left;' : nil
-    content_tag(list_el, content, class: wrap_class.join(' '), style: wrap_style)
+    content_tag(list_el, content, class: 'tags-cloud', style: (style == :simple_cloud ? 'text-align: left;' : ''))
   end
 
   def link_to_issue_filter(title, filters, extra = {})
@@ -311,11 +307,9 @@ module TagsHelper
   def add_tags(style, tags, content, item_el, options)
     items = []
     tag_cloud tags, (1..8).to_a do |tag, weight|
-      items << content_tag(
-        item_el,
-        render_issue_tag_link(tag, options),
-        class: "tag-nube-#{weight}"
-      )
+      items << content_tag(item_el, render_issue_tag_link(tag, options),
+                           class: "tag-nube-#{weight}",
+                           style: (style == :simple_cloud ? 'font-size: 1em;' : ''))
     end
     separator = style == :simple_cloud ? tag_separator : ' '
     content << safe_join(items, separator)
