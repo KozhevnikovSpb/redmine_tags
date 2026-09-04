be9fbbbb8b8c21d8ad0be8ba3cb27a26ca333642
KozhevnikovSPB <57673312+KozhevnikovSpb@users.noreply.github.com>
2026-09-04 07:38:20 +0300

    v0.0.6: tag_color_field helper

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,6 +1,17 @@
 module TagsHelper
   include Redmineup::TagsHelper
 
+  def tag_color_field(tag)
+    stored = RedmineupTags.normalize_stored_color(tag.respond_to?(:color) ? tag.color : nil)
+    auto = RedmineupTags.auto_tag_color(tag)
+    muted_auto = RedmineupTags.mute_hex(auto)
+    render partial: 'tags/color_editor', locals: {
+      stored_hex: stored,
+      auto_hex: auto,
+      muted_auto_hex: muted_auto
+    }
+  end
+
   def render_issue_tag_link(tag, options = {})
     filters = issue_filters_for_tag_link(tag, options)
 
