8e0ee452ae0b8d66149358caa44c102d83acacee
KozhevnikovSPB <57673312+KozhevnikovSpb@users.noreply.github.com>
2026-09-07 23:53:46 +0300

    Permit toggle_system_visible on manage_tag_clouds.

--- init.rb
+++ init.rb
@@ -38,7 +38,7 @@ Redmine::Plugin.register :redmineup_tags do
       tag_cloud_preferences: %i[toggle edit update]
     }
     permission :manage_tag_clouds, {
-      tag_clouds: %i[index new create edit update destroy reorder preview],
+      tag_clouds: %i[index new create edit update destroy reorder preview toggle_system_visible],
       tag_cloud_preferences: %i[toggle edit update]
     }, require: :member
   end
