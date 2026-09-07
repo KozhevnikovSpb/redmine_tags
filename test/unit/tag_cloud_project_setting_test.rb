c0a2b46d5bc6fec2b0d06379e3f42c4f81fea5f4
KozhevnikovSPB <57673312+KozhevnikovSpb@users.noreply.github.com>
2026-09-08 00:18:30 +0300

    Cover ancestor lock of the system Tags cloud.

--- test/unit/tag_cloud_project_setting_test.rb
+++ test/unit/tag_cloud_project_setting_test.rb
@@ -41,4 +41,37 @@ class TagCloudProjectSettingTest < ActiveSupport::TestCase
     TagCloudPreference.reset_for_user!(@user, @project)
     assert_not TagCloudPreference.system_visible_for?(@user, @project)
   end
+
+  test 'visible by default forces include_subprojects off' do
+    skip unless TagCloudProjectSetting.table_exists?
+    skip unless TagCloudProjectSetting.include_subprojects_column?
+
+    TagCloudProjectSetting.update_system!(@project, visible: true, include_subprojects: true)
+    assert TagCloudProjectSetting.local_visible_by_default?(@project)
+    assert_not TagCloudProjectSetting.local_include_subprojects?(@project)
+  end
+
+  test 'parent hide with subprojects locks descendants' do
+    skip unless TagCloudProjectSetting.table_exists?
+    skip unless TagCloudProjectSetting.include_subprojects_column?
+
+    child = projects(:projects_003)
+    child.stubs(:ancestors).returns([@project])
+
+    TagCloudProjectSetting.update_system!(@project, visible: false, include_subprojects: true)
+    assert TagCloudProjectSetting.locked_by_ancestor?(child)
+    assert_not TagCloudProjectSetting.system_visible_by_default?(child)
+  end
+
+  test 'parent hide without subprojects does not lock descendants' do
+    skip unless TagCloudProjectSetting.table_exists?
+    skip unless TagCloudProjectSetting.include_subprojects_column?
+
+    child = projects(:projects_003)
+    child.stubs(:ancestors).returns([@project])
+
+    TagCloudProjectSetting.update_system!(@project, visible: false, include_subprojects: false)
+    assert_not TagCloudProjectSetting.locked_by_ancestor?(child)
+    assert TagCloudProjectSetting.system_visible_by_default?(child)
+  end
 end
