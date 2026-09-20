dfcee14368626b4ac21d1a3a740d639de1505091
KozhevnikovSPB <57673312+KozhevnikovSpb@users.noreply.github.com>
2026-09-20 13:05:10 +0300

    Apply agreed plugin list and General hint copy

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ TAGS_VERSION_TYPE = 'Development'
 Redmine::Plugin.register :redmineup_tags do
   name 'Redmine Multi Tags Clouds plugin'
   author 'RedmineUP / KozhevnikovSpb'
-  description 'Plugin: issue tags and extra tag clouds in the project sidebar'
+  description 'This is an issue tags and tag clouds plugin for Redmine'
   version TAGS_VERSION_NUMBER
   url 'https://github.com/KozhevnikovSpb/redmine_tags'
   author_url 'mailto:support@redmineup.com'
